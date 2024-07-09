57a0ab2ecc67506cbe1f8deac117490af3b74cb3
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-09 14:17:33 +0530

    Logo made beautiful

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,17 @@
-import { Box, HStack } from '@chakra-ui/react'
+import { HStack, Heading } from '@chakra-ui/react'
 import React from 'react'
+import { IoDiamondOutline } from 'react-icons/io5'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <HStack p={4} justifyContent={'space-between'} alignItems={'center'} backgroundColor={'#25282c'}>
-            <Box>
-                Deependra Parmar 
-            </Box>
+        <HStack p={3} justifyContent={'space-between'} alignItems={'center'} backgroundColor={'#25282c'}>
+            <Link to={'/'}>
+                <HStack>
+                    <IoDiamondOutline color='gold' size={'18'} />
+                    <Heading fontFamily={'Sofia'} fontSize={'xl'}>Deependra Parmar...</Heading>
+                </HStack>
+            </Link>
             <HStack gap={4}>
                 <Link to="/">Home</Link>
                 <Link to="/skills">Skills</Link>
