f2c7ba507d04e918428f4468bb05eac1986820f3
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-14 18:12:33 +0530

    Added all the project

--- pages/Projects.jsx
+++ pages/Projects.jsx
@@ -1,7 +1,6 @@
 import { Image } from '@chakra-ui/image';
 import { AspectRatio, Box, Button, Container, Flex, HStack, Icon, SimpleGrid, Tag, Text, Tooltip, VStack, useColorModeValue } from '@chakra-ui/react';
 import React from 'react';
-import { AiOutlineStar } from 'react-icons/ai';
 import { BsFillRecordFill } from 'react-icons/bs';
 import { FiGithub } from 'react-icons/fi';
 import TransitionWrapper from '../components/Transition';
@@ -18,7 +17,6 @@ import wordx from "../src/assets/projects/09_wordx.png";
 import yuvaantar from "../src/assets/projects/10_yuvaantar.png";
 import todo from "../src/assets/projects/11_todo.png";
 import dashboard from "../src/assets/projects/12_admindashboard.png";
-import proton from "../src/assets/projects/13_proton.png";
 import maxcoach from "../src/assets/projects/14_maxcoach.png";
 
 const Projects = () => {
@@ -48,93 +46,85 @@ const Projects = () => {
     live: 'https://twitter-dp.vercel.app',
   },
   {
-    title: 'Todo with Auth',
-    description: 'This is a notebook app which will help you to crate notes for your daily work with authentication.',
-    cover: todo,
-    techStack: ['HTML', 'CSS', 'JS', 'MongoDB'],
-    url: 'https://github.com/DeependraParmar/Todo-Frontend',
+    title: 'Deependra Parmar 2.0',
+    description: 'This is my personal portfolio website showcasing my skills, expertise, projects, achievements and more.',
+    cover: portfolio1,
+    techStack: ['ChakraUI', 'React', 'Vite'],
+    url: 'https://github.com/DeependraParmar/Deependra-Parmar-Portfolio',
+    live: 'https://deependraparmar.vercel.app/',
+  },
+  {
+    title: 'Deependra Parmar 1.0',
+    description: 'This is my personal portfolio website showcasing my skills, expertise, projects, achievements and more.',
+    cover: portfolio2,
+    techStack: ['ReactJS', 'SaSS'],
+    url: 'https://github.com/DeependraParmar/DeependraParmarPortfolio',
+    live: 'https://deependra-parmar.vercel.app/',
+  },
+  {
+    title: 'CryptoGraphix',
+    description: 'A Crypto-application which shows all the coins, exchanges, their real-time prices in multi-currency formats..',
+    cover: cryptographix,
+    techStack: ['ReactJS', 'Axios', 'CoinGecko'],
+    url: 'https://github.com/DeependraParmar/CryptoGraphix',
+    live: 'https://cryptographix.vercel.app',
+  },
+  {
+    title: 'AwesomeUI',
+    description: 'This is a frontend of a UI Library website which provides amazing re-usable components of ReactJS.',
+    cover: awesomeui,
+    techStack: ['HTML', 'TailWindCSS'],
+    url: 'https://github.com/DeependraParmar/AwesomeUi',
     live: 'https://ultimatetodo.vercel.app/',
   },
   {
-    title: 'Todo with Auth',
-    description: 'This is a notebook app which will help you to crate notes for your daily work with authentication.',
-    cover: todo,
-    techStack: ['HTML', 'CSS', 'JS', 'MongoDB'],
-    url: 'https://github.com/DeependraParmar/Todo-Frontend',
-    live: 'https://ultimatetodo.vercel.app/',
+    title: 'BM Developers',
+    description: 'BM Developers is a website for a software-developer firm made in ReactJS with fully-responsive frontend.',
+    cover: bmdevs,
+    techStack: ['ReactJS', 'TailwindCSS', 'SaSS'],
+    url: 'https://github.com/DeependraParmar/BM-Developers',
+    live: 'https://bmdevelopers.netlify.app/',
   },
   {
-    title: 'Todo with Auth',
-    description: 'This is a notebook app which will help you to crate notes for your daily work with authentication.',
-    cover: todo,
+    title: 'IPHM',
+    description: 'Informatics Practices Hindi Me is the website for my first youtube channel for notes, resources and more.',
+    cover: iphm,
     techStack: ['HTML', 'CSS', 'JS', 'MongoDB'],
-    url: 'https://github.com/DeependraParmar/Todo-Frontend',
-    live: 'https://ultimatetodo.vercel.app/',
+    url: 'https://github.com/DeependraParmar/InformaticsPracticesHindiMe',
+    live: 'https://learnlogics.github.io',
   },
   {
-    title: 'Todo with Auth',
-    description: 'This is a notebook app which will help you to crate notes for your daily work with authentication.',
-    cover: todo,
-    techStack: ['HTML', 'CSS', 'JS', 'MongoDB'],
-    url: 'https://github.com/DeependraParmar/Todo-Frontend',
-    live: 'https://ultimatetodo.vercel.app/',
+    title: 'WordX',
+    description: 'WordX is a powerful text-based tranformation tool which allows multiple utility with extension based file downloading.',
+    cover: wordx,
+    techStack: ['ReactJS', 'Bootstrap'],
+    url: 'https://github.com/DeependraParmar/WordX',
+    live: 'https://wordxbydp.netlify.app/',
   },
   {
-    title: 'Todo with Auth',
-    description: 'This is a notebook app which will help you to crate notes for your daily work with authentication.',
-    cover: todo,
-    techStack: ['HTML', 'CSS', 'JS', 'MongoDB'],
-    url: 'https://github.com/DeependraParmar/Todo-Frontend',
-    live: 'https://ultimatetodo.vercel.app/',
+    title: 'Yuvaantar',
+    description: 'Yuvaantar is a tech-based club at CSIT@AITR. This is the website for the clubs showing events and registration links.',
+    cover: yuvaantar,
+    techStack: ['ReactJS', 'SaSS'],
+    url: 'https://github.com/DeependraParmar/Yuvaantar',
+    live: 'https://yuvaantar.vercel.app/',
   },
   {
-    title: 'Todo with Auth',
-    description: 'This is a notebook app which will help you to crate notes for your daily work with authentication.',
-    cover: todo,
-    techStack: ['HTML', 'CSS', 'JS', 'MongoDB'],
-    url: 'https://github.com/DeependraParmar/Todo-Frontend',
-    live: 'https://ultimatetodo.vercel.app/',
+    title: 'EzyCart: An E-commerce Application',
+    description: "This is an e-commerce application's Admin dashboard with product listings, charts and more.",
+    cover: dashboard,
+    techStack: ['ReactJS', 'TypeScript', 'MERN'],
+    url: 'https://github.com/DeependraParmar/EzyCart-UI',
+    live: 'https://ezy-cart.vercel.app/admin/dashboard',
   },
   {
-    title: 'Todo with Auth',
-    description: 'This is a notebook app which will help you to crate notes for your daily work with authentication.',
-    cover: todo,
-    techStack: ['HTML', 'CSS', 'JS', 'MongoDB'],
-    url: 'https://github.com/DeependraParmar/Todo-Frontend',
-    live: 'https://ultimatetodo.vercel.app/',
-  },
-  {
-    title: 'Todo with Auth',
-    description: 'This is a notebook app which will help you to crate notes for your daily work with authentication.',
-    cover: todo,
-    techStack: ['HTML', 'CSS', 'JS', 'MongoDB'],
-    url: 'https://github.com/DeependraParmar/Todo-Frontend',
-    live: 'https://ultimatetodo.vercel.app/',
-  },
-  {
-    title: 'Todo with Auth',
-    description: 'This is a notebook app which will help you to crate notes for your daily work with authentication.',
-    cover: todo,
-    techStack: ['HTML', 'CSS', 'JS', 'MongoDB'],
-    url: 'https://github.com/DeependraParmar/Todo-Frontend',
-    live: 'https://ultimatetodo.vercel.app/',
-  },
-  {
-    title: 'Todo with Auth',
-    description: 'This is a notebook app which will help you to crate notes for your daily work with authentication.',
-    cover: todo,
-    techStack: ['HTML', 'CSS', 'JS', 'MongoDB'],
-    url: 'https://github.com/DeependraParmar/Todo-Frontend',
-    live: 'https://ultimatetodo.vercel.app/',
-  },
-  {
-    title: 'Todo with Auth',
-    description: 'This is a notebook app which will help you to crate notes for your daily work with authentication.',
-    cover: todo,
-    techStack: ['HTML', 'CSS', 'JS', 'MongoDB'],
-    url: 'https://github.com/DeependraParmar/Todo-Frontend',
-    live: 'https://ultimatetodo.vercel.app/',
-  },
+    title: 'MaxCoach Landing Page',
+    description: 'This is the landing page for MaxCoach. com which I created when I was learning HTML & CSS',
+    cover: maxcoach,
+    techStack: ['HTML', 'CSS'],
+    url: 'https://github.com/DeependraParmar/MaxCoach-Internship',
+    live: 'https://maxcoach-internship.vercel.app/',
+  }
   ];
 
   return (
