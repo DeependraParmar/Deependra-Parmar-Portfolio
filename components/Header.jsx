40912e60be67c68f3c0c034f75c815e28b927341
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-14 18:29:18 +0530

    Added icons in the header's button

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,7 @@
 import { Button, HStack, Heading } from '@chakra-ui/react'
 import React from 'react'
-import { IoDiamondOutline } from 'react-icons/io5'
+import { IoDiamondOutline, IoDocument } from 'react-icons/io5'
+import { TbBriefcaseFilled } from 'react-icons/tb'
 import { Link } from 'react-router-dom'
 import TransitionWrapper from './Transition'
 
@@ -15,8 +16,8 @@ const Header = () => {
                     </HStack>
                 </Link>
                 <HStack gap={2}>
-                    <Link to="mailto:deependraparmar1@gmail.com" target='_blank'><Button background='purple.100' color={'white'} size={['xs', 'sm', 'sm', 'sm']} _hover={{ backgroundColor: 'purple.200' }}>Hire Me</Button></Link>
-                    <Link to="/"><Button size={['xs', 'sm', 'sm', 'sm']}>Resume</Button></Link>
+                    <Link to="mailto:deependraparmar1@gmail.com" target='_blank'><Button background='purple.100' color={'white'} size={['xs', 'sm', 'sm', 'sm']} _hover={{ backgroundColor: 'purple.200' }} gap={2}>Hire Me <TbBriefcaseFilled /></Button></Link>
+                    <Link to="/"><Button size={['xs', 'sm', 'sm', 'sm']} gap={1}>Resume <IoDocument /></Button></Link>
                 </HStack>
             </HStack>
         </TransitionWrapper>
